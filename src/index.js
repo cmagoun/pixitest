06fb283c6cd88d569228a33a9a4e643ef2d5f475
Chris Magoun <cmagoun@verizon.net>
2018-09-08 01:08:27 -0400

    fix merge

--- src/index.js
+++ src/index.js
@@ -46,10 +46,9 @@ function setup() {
     function gameLoop() {
         requestAnimationFrame(gameLoop);
         app.render(app.stage);
-<<<<<<< HEAD
+
         //actually run a game loop that manages game state
-=======
->>>>>>> master
+
         gm.runAnimations();
     }
 
